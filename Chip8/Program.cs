de6c142bb2f3654eb61059a5d0a25ac0aed7bc2d
CLown1331 <clown1331@yahoo.com>
2019-07-31 12:24:02 +0600

    init

---
+++ Chip8/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Emulator;
+
+namespace Chip8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Chip8Emulator emulator = new Chip8Emulator();
+        }
+    }
+}
---
+++ Emulator/Chip8Emulator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Emulator
+{
+    public class Chip8Emulator
+    {
+        public Chip8Emulator()
+        {
+
+        }
+    }
+}
